4a637856af5ac20ba9d03043f95439608fe1f28d
Christian Alvin Buhat <56921590+alvinbuhat@users.noreply.github.com>
2022-09-02 13:40:20 -0500

    Update HW1Exer3.py

--- HW1/HW1Exer3.py
+++ HW1/HW1Exer3.py
@@ -13,7 +13,7 @@ import scipy.io
 plt.rcParams['figure.figsize'] = [10, 10]
 plt.rcParams.update({'font.size': 18})
 
-mat_contents = scipy.io.loadmat(os.path.join('allFaces.mat'))
+mat_contents = scipy.io.loadmat(os.path.join('allFaces.mat')) #Load the Yale B image
 faces = mat_contents['faces']
 m = int(mat_contents['m'])
 n = int(mat_contents['n'])
@@ -21,6 +21,7 @@ nfaces = np.ndarray.flatten(mat_contents['nfaces'])
 
 allPersons = np.zeros((n*6,m*6))
 count = 0
+
 
 for j in range(6):
     for k in range(6):
@@ -32,4 +33,11 @@ img.set_cmap('gray')
 plt.axis('off')
 plt.show()
 
-U, S, VT = np.linalg.svd(allPersons, full_matrices=False)
+
+U, S, VT = np.linalg.svd(allPersons, full_matrices=False) #compute the economy SVD
+
+#Compare the first 10 left singular vectors using each method
+
+#Compare further vectors
+
+#Explain

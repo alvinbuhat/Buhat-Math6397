7f8d4c42c74cb759cac358e38a12548f61fced99
Christian Alvin Buhat <56921590+alvinbuhat@users.noreply.github.com>
2022-09-02 13:24:19 -0500

    Update HW1Exer4.py

--- HW1/HW1Exer4.py
+++ HW1/HW1Exer4.py
@@ -11,22 +11,34 @@ import numpy as np
 #import os
 plt.rcParams['figure.figsize'] = [16, 8] #matlab operator set size of image
 
-
+#random100x100 matrix
 A = np.random.normal(size=(100,100))
 
 U, S, VT = np.linalg.svd(A, full_matrices=True)
 
-
+#compute the SVD of the matrix and plot the singular values
 plt.boxplot(np.array([np.linalg.svd(np.random.normal(size=(100,100)), full_matrices=True)[1] for i in range(100)]))
 
+#function to get the median of sigmas
+def smedian(S):
+        res = []
+        for i in range( S.shape[0]):
+            res.append(np.median(S[:i+1]))
+        return res
+    
 
+#plot of mean and median for different matrix size
 for i in (50, 200, 500, 1000):
-    S = np.linalg.svd(np.random.normal(size=(i,i)), full_matrices=True)[1]
+    #addl defns
+    S1 = np.linalg.svd(np.random.normal(size=(i,i)), full_matrices=True)[1]
+    u = len(S1)
+    rng = np.arange(1, u + 1)
+
     plt.figure(i)
     plt.title(str(i)+' by' + str(i))
     plt.xlabel('r')
-    mean = np.cumsum(S)/np.arange(1,len(S)+1)
+    mean = np.cumsum(S1)/rng #command to get the mean 
     plt.plot(mean, label='mean')
-    median = np.median(S) #error
-    plt.plot(median, label='median')
+    median =  smedian(S1) 
+    plt.plot(rng, median, label='median')
     plt.legend()

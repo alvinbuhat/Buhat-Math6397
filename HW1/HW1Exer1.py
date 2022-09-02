54fe38bc980723803e7804202c4c3a7a9ffead79
Christian Alvin Buhat <56921590+alvinbuhat@users.noreply.github.com>
2022-09-02 13:27:04 -0500

    Update HW1Exer1.py

--- HW1/HW1Exer1.py
+++ HW1/HW1Exer1.py
@@ -13,14 +13,14 @@ import os
 plt.rcParams['figure.figsize'] = [16, 8] #matlab operator set size of image
 
 
-A = imread(os.path.join('dog.jpg')) #load the image
-X = np.mean(A, -1); #mean of array from the image #mean around -1
-# Convert RGB to grayscale
+A = imread(os.path.join('dog.jpg')) #load the image of the dog
+X = np.mean(A, -1); 
+
 
 U, S, VT = np.linalg.svd(X,full_matrices=True)
 S = np.diag(S)
 
-r = 500 #any number less than 1500
+r = 500 #any number less than 1500 (Chosen rank < m)
 
 I = np.linalg.inv(U[:r,:r]) @ U[:r,:r]
 J = U[:r,:r] @ np.linalg.inv(U[:r,:r])
@@ -32,12 +32,14 @@ print('the SVD is U=', U)
 print('S=', S)
 print('VT=', VT)
 print('We choose r=',r)
+#Showing U*U = I
 print('So we have the identity matrix from U*U=',I)
+#Showing U*U != UU*
 print('However, UU* doesnt generate an identity matrix as shown:',J)
 
 print('with error', K)
 
-
+#error plot
 C = [np.linalg.norm(U[:r,:r] @ np.linalg.inv(U[:r,:r]) - np.eye(r)) for r in range(U.shape[0])]
 
 plt.yscale('log')

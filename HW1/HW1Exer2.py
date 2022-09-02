9f61da113baa0473ac0baf9f3f31396a80a30264
Christian Alvin Buhat <56921590+alvinbuhat@users.noreply.github.com>
2022-09-02 13:33:54 -0500

    Update HW1Exer2.py

--- HW1/HW1Exer2.py
+++ HW1/HW1Exer2.py
@@ -13,26 +13,27 @@ import os
 plt.rcParams['figure.figsize'] = [16, 8] #matlab operator set size of image
 
 
-A = imread(os.path.join('dog.jpg')) #load the image
+A = imread(os.path.join('dog.jpg')) #load the image of the dog
 X = np.mean(A, -1); 
 
+#compute the economy SVD
 U, S, VT = np.linalg.svd(X,full_matrices=False)
 S = np.diag(S)
 
+#actual data value
 x_0 = np.linalg.norm(A)
 
-RE = [abs(((np.linalg.norm(U[:,:r] @ S[0:r,:r] @ VT[:r,:]))/x_0) - 1) for r in range(1,len(S))]
+#relative error
+RE = [abs((np.linalg.norm(U[:,:r] @ S[0:r,:r] @ VT[:r,:])-x_0)/x_0) for r in range(1,len(S))]
 
 
 
-
-plt.title('Relative Reconstruction error, Missed Variance and Cumulative Sum')
+#plot part
 plt.xlabel('r')
 plt.plot(1 - np.array(RE), color='blue', label='Relative Reconstruction Error')
 plt.plot(1 - (np.array(RE)**2), color='red', label='Variance')
 plt.plot(np.cumsum(np.diag(S))/np.sum(np.diag(S)), color='black', label='Cumulative Sum')
 plt.legend();
 
-plt.tight_layout()
 
 print('RE recaptures the 99% of variance when r is around 20, 99% of cumulative sum when r is at most 20, although note that they have different starting points')
